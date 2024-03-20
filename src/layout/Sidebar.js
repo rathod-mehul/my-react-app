c62d6effeb81cd9098d8e11fff1607779a650e75
rathod-mehul <rathod492005@gmail.com>
2024-03-20 09:44:39 +0530

    try routers concept

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,15 +1,45 @@
+import { propTypes } from "react-bootstrap/esm/Image";
+import Dashboard from "../content/Dashboard";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import StyleComponent from '../practice_folder/style/StyleCompont';
+
+
 function Slidebar(props){
     return(
         // <div>this is a Slidebar {props.menu[1]} </div>
         <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-white">
         <div className="position-sticky">
           <div className="list-group list-group-flush mx-3 mt-4">
+           <div>
+            <Link to="/">
             <a href="#" className="list-group-item list-group-item-action py-2" data-mdb-ripple-init aria-current="true">
               <i className="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
             </a>
+            </Link>
+
+            <Link to="/style">
             <a href="#" className="list-group-item list-group-item-action py-2 active" data-mdb-ripple-init>
               <i className="fas fa-chart-area fa-fw me-3"></i><span>Website traffic </span>
             </a>
+            </Link>
+
+            <Routes>
+                        <Route
+                            path="/"
+                            element={<Dashboard />}
+                        ></Route>
+                        <Route
+                            path="/style"
+                            element={<StyleComponent />}
+                        ></Route>
+                    </Routes>
+           </div>
+           
             <a href="#" className="list-group-item list-group-item-action py-2" data-mdb-ripple-init><i
                 className="fas fa-lock fa-fw me-3"></i><span>Password</span></a>
             <a href="#" className="list-group-item list-group-item-action py-2" data-mdb-ripple-init><i
@@ -34,5 +64,6 @@ function Slidebar(props){
       </nav>
     )
 }
+import { Form } from "react-router-dom";
 
 export default Slidebar;
